16756e11e43a375907727f1343b9150cdf020aef
Ivan <88632398+KaskaderVanek@users.noreply.github.com>
2022-11-21 17:52:33 +0300

    Update update-category.dto.ts

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,21 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger/dist'
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator'
 
 export class UpdateCategoryDto {
-  @ApiProperty({ required: true })
-  readonly id: string
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @Matches('^[a-zA-Z0-9_]*$', '', {
+    message: 'Slug должен быть строкой и не содержать кириллицу',
+  })
+  @IsNotEmpty({ message: 'Slug не может быть пустым' })
   readonly slug?: string
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString({ message: 'Name должен быть строкой' })
+  @IsNotEmpty({ message: 'Name не может быть пустым' })
   readonly name?: string
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString({ message: 'Description должен быть строкой' })
   readonly description?: string
 
   @ApiProperty({ required: false })
-  readonly createdDate?: Date
-
-  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsBoolean({ message: 'Active должен быть типа Boolean' })
   readonly active?: boolean
 }
