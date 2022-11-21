cbfeabdfbe5bf4b8d8d0d599add7508b72b11c5f
Ivan <88632398+KaskaderVanek@users.noreply.github.com>
2022-11-21 17:53:13 +0300

    Update categories.controller.ts

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -10,7 +10,7 @@ import {
   Query,
   Put,
   ValidationPipe,
-  UsePipes,
+  HttpException,
 } from '@nestjs/common'
 import {
   ApiOperation,
@@ -22,6 +22,8 @@ import { CreateCategoryDto } from './dto/create-category.dto'
 import { Category } from './categories.model'
 import { CategoriesService } from './categories.service'
 import { UpdateCategoryDto } from './dto/update-category.dto'
+import { UuidValidationPipe } from 'src/pipes/uuid-validation.pipe'
+import { FilterCategory } from './dto/filter-category.dto'
 
 @ApiTags('Categories')
 @Controller('categories')
@@ -32,38 +34,18 @@ export class CategoriesController {
   @ApiResponse({ status: 201, type: Category })
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  createCategory(
-    @Body(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-      }),
-    )
-    dto: CreateCategoryDto,
-  ): Promise<Category> {
+  createCategory(@Body(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true})) dto: CreateCategoryDto): Promise<Category> {
     return this.categoryService.create(dto)
   }
 
   @ApiOperation({ summary: 'Изменить категорию' })
   @ApiResponse({ status: 200 })
   @Put(':id')
-  updateCategory(@Param('id') id: string, @Body() dto: UpdateCategoryDto) {
+  updateCategory(
+      @Param('id', UuidValidationPipe) id: string, 
+      @Body(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true})) dto: UpdateCategoryDto
+    ): Promise<Category> {
     return this.categoryService.update(id, dto)
-  }
-
-  @ApiOperation({ summary: 'Получить категорию по Id или Slug', description: 'В поле {value} нужно передать id или slug' })
-  @ApiParam({name: 'value', example: 'a954927f-2ac6-4b9d-8486-03c0e8616ff3'})
-  @ApiResponse({ status: 200, type: Category, })
-  @Get(':value')
-  getByIdOrSlug(@Param('value') value: string): Promise<Category> {
-    return this.categoryService.getOne(value)
-  }
-
-  @ApiOperation({ summary: 'Удалить категорию' })
-  @ApiResponse({ status: 200 })
-  @Delete(':id')
-  deleteCategory(@Param('id') id: string) {
-    return this.categoryService.delete(id)
   }
 
   @ApiOperation({ summary: 'Получить все категории' })
@@ -72,4 +54,32 @@ export class CategoriesController {
   getAllCategories(): Promise<Category[]> {
     return this.categoryService.getAll()
   }
+  
+  @ApiOperation({ summary: 'Получить массив категорий по фильтру' })
+  @ApiResponse({ status: 200 })
+  @Get('filter')
+  filterCategory(@Query(new ValidationPipe({transform: true, whitelist: false, forbidNonWhitelisted: true})) filter: FilterCategory) {
+    return this.categoryService.filter(filter)
+  }
+
+  @ApiOperation({
+    summary: 'Получить категорию по Id или Slug',
+    description: 'В поле {value} нужно передать id или slug',
+  })
+  @ApiParam({ name: 'value', example: 'a954927f-2ac6-4b9d-8486-03c0e8616ff3' })
+  @ApiResponse({ status: 200, type: Category })
+  @Get(':value')
+  getByIdOrSlug(@Param('value') value: string): Promise<Category> {
+    return this.categoryService.getOne(value)
+  }
+
+  @ApiOperation({ summary: 'Удалить категорию' })
+  @ApiResponse({ status: 200 })
+  @Delete(':id')
+  deleteCategory(@Param('id', UuidValidationPipe) id: string): Promise<HttpException> {
+    return this.categoryService.delete(id)
+  }
+
+
+
 }
